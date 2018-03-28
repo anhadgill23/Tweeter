f93664fd17eb4f34b8c7c223c374e69db1d9d0b9
Anhad Gill <gillanhad23@gmail.com>
2018-03-28 00:42:39 +0000

    Adds compose tweet container and tweets container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function () {
+    // console.log('this inside document.ready:', this);
+    $('textarea').on ('keypress',  function () {
+    var $input= 10 - $(this).val().length
+    var $charCount = $(this).siblings('.counter')
+    $charCount.text($input);
+
+    if ($input < 0) {
+        $charCount.css('color', 'red')
+    }
+    });
+})
+
+/*
+Tip: You could re-target the textarea using the same selector you've used in this file to
+ establish the event listener, but you already have a handle on the textarea thanks to this. 
+ But this points to a plain DOM node meaning it doesn't have any of the nice jQuery 
+ functions like .val() to get the value of an textarea. How can you leverage this while 
+ also having access to these jQuery niceties ?
+
+By the end of this task you should be console logging the number of characters left. And 
+if the textarea has more than 140 characters it should be logging negative values.*/
